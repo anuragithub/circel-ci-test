83c36fc10e7d8be072f774952a11edbd25c37a87
anuragithub <mishraanu.mishra7@gmail.com>
2020-09-23 17:38:35 +0530

    Added tests to test circleci

---
+++ .circleci/main_project/tests/cci_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+import numpy
+import pandas
+
+import pycorrcat.pycorrcat as pycorrcat
+
+
+class TestCircleCI(unittest.TestCase):
+    def test_functions(self):
+        self.assertTrue(1 == 1)
+        return
